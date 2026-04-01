8bd7898269769dfbf06f3eaa9b567935b1801606
Deepak Kumar <dkprajapati20012004@gmail.com>
2026-04-01 23:43:51 +0530

    Financial Record Model

---
+++ src/models/record.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const recordSchema = new mongoose.Schema(
+  {
+    amount: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["income", "expense"],
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+    notes: {
+      type: String,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Record = mongoose.model("Record", recordSchema);
+
+export default Record;
